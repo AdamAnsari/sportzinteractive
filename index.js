48a8c5f58d82023cac3f11e6ff92c395df1f9425
Adam Ansari <adamansari@Adams-MacBook-Air.local>
2021-05-02 03:00:24 +0530

    Structure completed

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
+var playerListArr = [];
+var playerListObj = {};
+
 const userAction = async () => {
   const response = await fetch('https://api.npoint.io/20c1afef1661881ddc9c');
   const myJson = await response.json(); //extract JSON from the http response
   console.log(myJson);
-
-  var playerListArr = [];
-  var playerListObj = {};
-  
-  myJson.playerList.forEach((element, index) => {
+  myJson.playerList.forEach((element) => {
     playerListObj = {
       Id: element.Id,
       PFName: element.PFName,
@@ -18,7 +17,49 @@ const userAction = async () => {
     }
     playerListArr.push(playerListObj);
     playerListObj = {}
-  });
-  console.log(playerListArr);
+  }); 
+
+  this.playerListArr = this.playerListArr.sort((a, b) => (+a.Value < +b.Value ? -1 : 1));
+
+  console.log(playerListArr); 
+  displayCard();
 }
 userAction();
+
+const displayCard = () => {
+  var main = document.getElementById("card-container");
+  var div = [];
+  const red = "Lorem ipsum dolor, sit amet consectetur adipisicing elit";
+  playerListArr.forEach((element, i) => {
+    div.push(document.createElement("div"));
+    div[i].setAttribute("class","card col-md-4 col-sm-4");
+    // div[i].setAttribute("style","width:18 rem;");
+    const image = document.createElement("img");
+    image.setAttribute("class","card-img-top");
+    image.src = `assets/player-images/${element.Id}.jpg`;;
+    div[i].appendChild(image);
+    var innerDiv = document.createElement("div");
+    innerDiv.setAttribute("class","card-body");
+    var h3 = document.createElement("h3");
+    h3.textContent = (element.PFName !== '') ? `Name: ${element.PFName}` : 'Name: Not Provided';
+    var h4 = document.createElement("h4");
+    h4.setAttribute("class","card-text");
+    h4.textContent = (element.SkillDesc !== '') ? `Skill: ${element.SkillDesc}` : 'Skill: Not Provided';
+    innerDiv.appendChild(h3);
+    innerDiv.appendChild(h4);
+    var h4 = document.createElement("h4");
+    h4.setAttribute("class","card-text");
+    h4.textContent = (element.Value !== '') ? `Value: $${element.Value}` : 'Value: Not Provided';
+    innerDiv.appendChild(h4);
+    var h4 = document.createElement("h4");
+    h4.setAttribute("class","card-text");
+    h4.textContent = (element.CCode !== '' || element.VsCCode !== '') ? `Upcoming Matches List: ${element.CCode} vs ${element.VsCCode}` : 'Upcoming Matches List: Not Provided';
+    innerDiv.appendChild(h4);
+    var p = document.createElement("p");
+    p.setAttribute("class","card-text");
+    p.textContent = (element.MDate !== '') ? 'Date: ' + new Date(element.MDate + ' UTC').toString() : 'Date: Not Provided';
+    innerDiv.appendChild(p);
+    div[i].appendChild(innerDiv);
+    main.appendChild(div[i]);
+  });
+}
